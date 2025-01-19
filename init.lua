0f406d8516a832209dfe5078eb7f0a694c3c60d1
apePetrus <shiveringveteran@proton.me>
2025-01-19 13:33:10 -0300

    Added lualine

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+require("vim-options")
 require("config.lazy")
-
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
