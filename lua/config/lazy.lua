46f1c7ca6040333bc12a73ae4fcd6a3dd1ee6ef8
apePetrus <shiveringveteran@proton.me>
2025-01-19 13:18:59 -0300

    Remade some config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,4 +1,3 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -15,24 +14,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-<<<<<<< HEAD
--- Setup lazy.nvim
-=======
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
->>>>>>> d026fe47b394c9be05c2632c8aa1768b50a5b0ff
 require("lazy").setup({
   spec = {
     { import = "plugins" },
   },
-<<<<<<< HEAD
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "catppuccin" } },
-  -- automatically check for plugin updates
-=======
-  install = { colorscheme = { "catppuccin" } },
->>>>>>> d026fe47b394c9be05c2632c8aa1768b50a5b0ff
   checker = { enabled = true },
 })
