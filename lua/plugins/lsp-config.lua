f55921c546d5b7a96e4b3f8c81c6752fbdad059e
apePetrus <shiveringveteran@proton.me>
2025-01-20 06:12:47 -0300

    Configured lsp and telescope to work with Python

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "intelephense", },
+                ensure_installed = { "lua_ls", "intelephense", "pylsp" },
             })
         end,
     },
@@ -19,7 +19,10 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.intelephense.setup({})
+            lspconfig.pylsp.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
         end,
     },
 }
