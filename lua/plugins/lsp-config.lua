9b3b45d488a8852b386ab80d6943dc54417df975
apePetrus <shiveringveteran@proton.me>
2025-01-30 20:18:03 -0300

    Added html to lsp config and none-ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.intelephense.setup({})
             lspconfig.pyright.setup({})
+            lspconfig.html_lsp.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,8 @@ return {
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.diagnostics.pyright,
             null_ls.builtins.formatting.pyright,
+            null_ls.builtins.diagnostics.html_lsp,
+            null_ls.builtins.formatting.html_lsp,
         })
 
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {});
