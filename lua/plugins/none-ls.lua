96c6f36a643a946f3973718bdcad07355e29d30e
apePetrus <shiveringveteran@proton.me>
2025-01-30 21:04:27 -0300

    Changed none-ls and added support for JS and Python LSPs. The none-ls builtin is deprecated I must fixthis. Also added neoscroll plugin for smooth scrolling. Added alpha to make a simple start menu.

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,14 +1,19 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
-
         null_ls.setup({
-            null_ls.builtins.formatting.stylua,
-            null_ls.builtins.diagnostics.pyright,
-            null_ls.builtins.formatting.pyright,
-            null_ls.builtins.diagnostics.html_lsp,
-            null_ls.builtins.formatting.html_lsp,
+            sources = {
+                require("none-ls.diagnostics.eslint_d"),
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.eslint_d,
+            }
         })
 
         vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {});
