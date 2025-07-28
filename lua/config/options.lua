bd600aef71f3ca96078d69982106b352d51bfd0a
Pedro Pereira <ppereira@softexpert.com>
2025-07-28 10:09:25 -0300

    first commit

---
+++ lua/config/options.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.g.autoformat = false
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set nowrap")
+vim.cmd("set noeol")
+vim.cmd("set nofixendofline")
