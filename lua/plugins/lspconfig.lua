7a0b165b700719d7745212ae7428a3fe20734257
Pedro Pereira <pedro.hr.pereira@proton.me>
2025-08-17 12:04:33 -0300

    FEAT: changed some lsp config to get more specific
    Also cleaning example.lua

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,23 +1,21 @@
 return {
     {
-        'williamboman/mason.nvim',
-        opts = {
-            ensure_installed = {
-                'eslint_d',
-                'eslint-lsp',
-                'typescript-language-server',
-                'intelephense',
-            },
-        },
-    },
-
-    {
         'nvim-treesitter/nvim-treesitter',
         opts = {
             ensure_installed = {
                 'typescript',
                 'javascript',
                 'php',
+                'lua',
+                'html',
+                'bash',
+                'markdown',
+                'markdown_inline',
+                'json',
+                'html',
+                'python',
+                'vim',
+                'yaml',
             },
         },
         vim.treesitter.language.register('php', 'inc'),
