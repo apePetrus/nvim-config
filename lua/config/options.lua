416c3f17e7505bc848ca1755897d933111eeae06
Pedro Pereira <ppereira@softexpert.com>
2025-10-27 09:53:18 -0300

    refactor(lua): change vim.cmd("set foo") to vim.opt.foo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,8 +7,9 @@ vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set nowrap")
-vim.cmd("set noeol")
-vim.cmd("set nofixendofline")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.eol = false
+vim.opt.fixendofline = false
