499fb6c09e3721d4a878584f744d2e5611894533
Pedro Pereira <ppereira@softexpert.com>
2025-10-31 12:31:54 -0300

    feat(indent): .inc and .php files use spaces over tabs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,6 @@ vim.g.maplocalleader = "\\"
 vim.g.autoformat = false
 vim.g.lazyvim_php_lsp = "intelephense"
 
-vim.opt.expandtab = false
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -13,3 +12,12 @@ vim.opt.relativenumber = true
 vim.opt.wrap = false
 vim.opt.eol = false
 vim.opt.fixendofline = false
+
+vim.opt.expandtab = false
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "php", "inc" },
+  callback = function()
+    vim.opt_local.expandtab = true
+  end,
+})
