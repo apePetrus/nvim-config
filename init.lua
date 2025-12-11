02fd14e1d4f38459c5cb150aef6dafa796978faf
Pedro Pereira <ppereira@softexpert.com>
2025-12-11 07:01:39 -0300

    telescope keybinds

--- init.lua
+++ init.lua
@@ -30,3 +30,10 @@ telescope.setup({
 	},
 })
 require('telescope').load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader><space>', builtin.find_files)
+vim.keymap.set('n', '<leader>/',       builtin.live_grep)
+vim.keymap.set('n', '<leader>,',       builtin.buffers)
+
