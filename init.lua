757b8a1a648742fff24f57c59de9360d68170307
Pedro Pereira <ppereira@softexpert.com>
2025-12-12 13:42:29 -0300

    adding intelephense for lsp, also dap, nvim-cmp and none-ls plugins

--- init.lua
+++ init.lua
@@ -25,12 +25,20 @@ vim.pack.add({
 	'https://github.com/chentoast/marks.nvim',
 	{ src = 'https://github.com/catppuccin/nvim', name = 'catppuccin', priority = 1000 },
 	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	'https://github.com/mfussenegger/nvim-dap',
+	'https://github.com/hrsh7th/nvim-cmp',
+	'https://github.com/hrsh7th/cmp-nvim-lsp',
+
+	'https://github.com/nvim-lua/plenary.nvim',
+	{ src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+	'https://github.com/nvim-telescope/telescope.nvim',
+
 	'https://github.com/neovim/nvim-lspconfig',
 	'https://github.com/mason-org/mason.nvim',
 	'https://github.com/mason-org/mason-lspconfig.nvim',
-	'https://github.com/nvim-lua/plenary.nvim',
-	{ src = 'https://github.com/nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-	'https://github.com/nvim-telescope/telescope.nvim'
+
+	'https://github.com/nvimtools/none-ls.nvim',
+	'https://github.com/jay-babu/mason-null-ls.nvim',
 })
 
 vim.cmd.colorscheme('catppuccin')
@@ -94,6 +102,28 @@ vim.api.nvim_create_autocmd("FileType", {
 require('mason').setup()
 require('mason-lspconfig').setup({
 	ensure_installed = {
-		'intelephense',
+		'intelephense', 'eslint', 'ts_ls'
 	},
+})
+
+require('mason-null-ls').setup({
+	ensure_installed = {
+		'php-cs-fixer', 'prettier', 'eslint_d', 'phpcs'
+	}
+})
+
+
+-- lspconfig
+vim.lsp.start({
+	name = 'intelephense',
+	cmd = { 'intelephense', '--stdio' },
+	root_dir = vim.fs.root(0, { 'composer.json', '.git' }),
+	on_attach = function(client, buffer)
+		local opts = { buffer = bufnr, silent = true }
+		map("n", "gd",         vim.lsp.buf.definition, opts)
+        map("n", "K",          vim.lsp.buf.hover, opts)
+        map("n", "gr",         vim.lsp.buf.references, opts)
+        map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+	end,
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
